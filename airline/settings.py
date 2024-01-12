7f7c1daa02f24b39a618abc2984768ef19b20a91
SS1mba <simeon.thomas238@gmail.com>
2024-01-11 21:20:00 -0500

    Vercel changes

--- airline/settings.py
+++ airline/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-$v%s$5=wv-r6zv@+wh+9(^fx)s=)nfe+hadh#=-t2i@l!)yogo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
--- airline/wsgi.py
+++ airline/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airline.settings')
 
 application = get_wsgi_application()
+
+app = application
